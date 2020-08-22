436ee9c691d039fd6919e9163b09412f15079369
Yuriuh <642192014@qq.com>
2020-08-22 22:39:54 +0800

    Display v-model

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { ToyVue as Vue } from './toy-vue.js'
 
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// })
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
 
 // var app2 = new Vue({
 //   el: '#app-2',
@@ -14,14 +14,14 @@ import { ToyVue as Vue } from './toy-vue.js'
 //   }
 // })
 
-var app5 = new Vue({
-  el: '#app-5',
-  data: {
-    message: 'Hello Vue.js!'
-  },
-  methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
-})
+// var app5 = new Vue({
+//   el: '#app-5',
+//   data: {
+//     message: 'Hello Vue.js!'
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
+// })
